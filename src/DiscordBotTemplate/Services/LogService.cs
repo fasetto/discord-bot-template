3bc1862320343dbd0a944d41d0a32e602d1bd995
fasetto <serkanbircan21@yandex.com>
2020-07-06 19:46:47 +0300

    Removed unnescessary usings and dependencies

--- src/DiscordBotTemplate/Services/LogService.cs
+++ src/DiscordBotTemplate/Services/LogService.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using Microsoft.Extensions.Configuration;
 
 namespace DiscordBotTemplate.Services
 {
